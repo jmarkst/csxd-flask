4e4f2ff1ddfc46a808d9a9478e7a691a6dba1bc7
jmarkst <enum.scima@gmail.com>
2024-12-15 12:56:47 +0800

    Fixed bug in video feature regarding conflict function and variable names. Removed dependency of video to upload.js

--- main.py
+++ main.py
@@ -107,14 +107,14 @@ def draw_box(frame, detection):
     # Render a smaller text size
     cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
 
-camera = None #cv2.VideoCapture(0)  # Capture from webcam
+camera = cv2.VideoCapture(0)  # Capture from webcam
 latest_detections = []  # To store the latest detections
 videoStartTime = 0
 videoFrames = 0
 
 latest_detections = []  # This will store the latest detection boxes
 frame_count = 0  # Keep track of frames
-skip_inference = 5  # Number of frames to skip between inferences
+skip_inference = 12 # Number of frames to skip between inferences
 
 def generate_frames():
     global latest_detections
--- static/js/video.js
+++ static/js/video.js
@@ -1,6 +1,61 @@
-var cache = [];
+var cacheV = [];
+var confidence = 0.7;
+var toShow = [true, true, true]
+const classes = [ 'broccoli', 'cabbage', 'cauliflower' ];
 
-async function fetchDetections() {
+function showDetections (boxes) {
+    const rsb = document.getElementById('right-sidebar');
+    rsb.innerHTML = ""
+
+    boxes.forEach(box => {
+        if (box.confidence >= confidence && toShow[box.class]) {
+            const container = document.createElement('div');
+            container.classList.add('margined', 'text-gray');
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const cardhead = document.createElement('div');
+            cardhead.classList.add('card-header');
+            const cardbody = document.createElement('div');
+            cardbody.classList.add('card-body');
+            const classpill = document.createElement('p');
+            classpill.classList.add('pill');
+            classpill.innerText = classes[ box.class ];
+            const h6 = document.createElement('h6');
+            h6.innerText = "INFORMATION"
+            const coordscont = document.createElement('div');
+            coordscont.classList.add('row', 'full');
+            const coordslabel = document.createElement('div');
+            coordslabel.classList.add('col', 'full', 'headtext');
+            coordslabel.innerText = "COORDINATES";
+            const coords = document.createElement('div');
+            coords.classList.add('col', 'full');
+            coords.innerText = `(${ box.x1 }, ${ box.x2 }, ${ box.y1 }, ${ box.y2})`;
+            const confcont = document.createElement('div');
+            confcont.classList.add('row', 'full');
+            const conflabel = document.createElement('div');
+            conflabel.classList.add('col', 'full', 'headtext');
+            conflabel.innerText = "CONFIDENCE";
+            const conf = document.createElement('div');
+            conf.classList.add('col', 'full');
+            conf.innerText = `${ (box.confidence * 100).toFixed(2) }%`;
+
+            // append children
+            coordscont.appendChild(coordslabel);
+            coordscont.appendChild(coords);
+            confcont.appendChild(conflabel);
+            confcont.appendChild(conf);
+            cardbody.appendChild(coordscont);
+            cardbody.appendChild(confcont);
+            cardhead.appendChild(classpill);
+            card.appendChild(cardhead);
+            card.appendChild(cardbody);
+            container.appendChild(card);
+            rsb.appendChild(container);
+        }
+    });
+}
+
+async function fetchDetectionsV() {
     try {
         console.log('Fetching detections...');
         const response = await fetch('/detections');
@@ -9,27 +64,41 @@ async function fetchDetections() {
 
         // Display detection information
         showDetections(data)
-        drawLeftSidebarCached(data)
-        //cache.push(data)
+        drawLeftSidebarCachedV(data)
+        //cacheV.push(data)
     } catch (error) {
         console.error('Error fetching detections:', error);
     }
 }
 
-function drawLeftSidebarCached(boxes) {
+function drawLeftSidebarCachedV(boxes) {
     const body = document.getElementById('left-body');
     body.innerHTML = "";
     const count = countDetections(boxes);
     for (let x = 0; x < count.length; x++) {
         if (count[ x ] != 0) {
             console.log(count)
-            console.log(cache[x])
-            drawInformationUsingCached(body, boxes, x)
+            console.log(cacheV[x])
+            drawInformationUsingCachedV(body, boxes, x)
         }
     }
 }
 
-function drawInformationUsingCached(body, boxes, veget) {
+function countDetections (boxes) {
+    var count = [0,0,0];
+    
+    boxes.forEach(box => {
+        if (box.confidence >= confidence && toShow[box.class]) {
+            count[ box.class ]++;
+        }
+    });
+
+    counted = count;
+
+    return count;
+}
+
+function drawInformationUsingCachedV(body, boxes, veget) {
     const classes = ["broccoli", "cabbage", "cauliflower"]
     const veg = [ 747447, 2346407, 2685573 ];
     const nuts = [
@@ -37,7 +106,7 @@ function drawInformationUsingCached(body, boxes, veget) {
         [1, 5, 9, 11, 12, 13, 15, 16, 17, 22, 23],
         [1, 5, 9, 12, 13, 14, 16, 17, 18]
     ];
-    console.log(cache[1].foodNutrients)
+    console.log(cacheV[1].foodNutrients)
     const card = document.createElement('div');
     card.classList.add('card');
     const header = document.createElement('div');
@@ -52,9 +121,9 @@ function drawInformationUsingCached(body, boxes, veget) {
     header.appendChild(vegetable);
     card.appendChild(header);
 
-    const link = cache[veget].fdcId;
+    const link = cacheV[veget].fdcId;
     const disclaimer = `<small><b>Data from USDA FoodCentral.</b><br>For a complete nutritional information,<b><a href='https://fdc.nal.usda.gov/fdc-app.html#/food-details/${link}/nutrients' target='_blank' rel='noopener noreferrer'>click here</a></b></small>`; // description
-    const fooditem = `<b>${cache[veget].description}</b>`
+    const fooditem = `<b>${cacheV[veget].description}</b>`
     cardbody.innerHTML = disclaimer + "<br>" + fooditem;
     const h6 = document.createElement('h6');
     h6.innerHTML = "NUTRITIONAL DATA";
@@ -63,7 +132,7 @@ function drawInformationUsingCached(body, boxes, veget) {
     for (var x = 0; x < nuts[veget].length; x++) {
         // Check if the nutrient exists in the response
         const nutrientIndex = nuts[ veget ][ x ];
-        if (!cache[veget].foodNutrients[ nutrientIndex ]) {
+        if (!cacheV[veget].foodNutrients[ nutrientIndex ]) {
             console.error(`Nutrient data missing at index ${ nutrientIndex }`);
             continue;
         }
@@ -73,12 +142,12 @@ function drawInformationUsingCached(body, boxes, veget) {
 
         const headtext = document.createElement('div');
         headtext.classList.add('col', 'full', 'headtext');
-        headtext.innerHTML = `${ cache[veget].foodNutrients[ nutrientIndex ].nutrient.name }`;
+        headtext.innerHTML = `${ cacheV[veget].foodNutrients[ nutrientIndex ].nutrient.name }`;
         row.appendChild(headtext);
 
         const value = document.createElement('div');
         value.classList.add('col', 'full');
-        value.innerHTML = `${ cache[veget].foodNutrients[ nutrientIndex ].amount } ${ cache[veget].foodNutrients[ nutrientIndex ].nutrient.unitName }`;
+        value.innerHTML = `${ cacheV[veget].foodNutrients[ nutrientIndex ].amount } ${ cacheV[veget].foodNutrients[ nutrientIndex ].nutrient.unitName }`;
         row.appendChild(value);
 
         cardbody.appendChild(row);
@@ -89,7 +158,7 @@ function drawInformationUsingCached(body, boxes, veget) {
     body.append(card);
 }
 
-async function cacheUSDAInfo() {
+async function cacheUSDAInfoV() {
     const veg = [ 747447, 2346407, 2685573 ];
     var usdaStart = performance.now()
     const loading = document.getElementById('loading');
@@ -107,7 +176,7 @@ async function cacheUSDAInfo() {
             });
             const data = await response.json();  // Await the JSON parsing as well
             console.log(data)
-            cache[x] = data
+            cacheV[x] = data
             loaded = `<br><small>${ x + 1 } cached</small>`
             loading.innerHTML = description + loaded;
         } catch (error) {
@@ -115,13 +184,13 @@ async function cacheUSDAInfo() {
         }
     }
     loading.classList.toggle('hide');
-    console.log(cache)
+    console.log(cacheV)
     var usdaEndTime = performance.now()
     var usdaTime = usdaEndTime - usdaStart
     console.log("USDA API caching time: " + usdaTime + "ms")
 }
 
-const logic = cacheUSDAInfo().then(() => {
+const logic = cacheUSDAInfoV().then(() => {
     console.log("cached OK")
-    setInterval(fetchDetections, 1000)
+    setInterval(fetchDetectionsV, 1000)
 })
